7adfb009be431175ed582acb3df4c98ec1fbe9f5
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-08-16 22:47:19 +0200

    updating eventlistener on clicking create poll

--- quick-polls-frontend/src/index.js
+++ quick-polls-frontend/src/index.js
@@ -4,13 +4,19 @@ const CLOSED_POLLS_URL = `${BASE_URL}/polls/closed`
 
 
 document.addEventListener("DOMContentLoaded", () => {
+    
+    // hiding dashboard and newsfeed when clicking on creat poll
     const create = document.getElementById("createPoll");
     create.addEventListener("click", () => {
         const panel = document.querySelector(".panel");
         if (panel.style.display === "none") {
             panel.style.display = "block";
+            document.querySelector("#createPoll h4").innerText = "Create Poll"
           } else {
             panel.style.display = "none";
+            document.querySelector("#createPoll h4").innerText = "Back to dashboard"
           }
     })
+
+    // 
 })
