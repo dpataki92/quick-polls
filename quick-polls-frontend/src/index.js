a47c44f9c9ffe8a16eaf17d01361b3299b1be48a
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-08-16 23:25:08 +0200

    handling create poll form displaying

--- quick-polls-frontend/src/index.js
+++ quick-polls-frontend/src/index.js
@@ -4,19 +4,23 @@ const CLOSED_POLLS_URL = `${BASE_URL}/polls/closed`
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    
-    // hiding dashboard and newsfeed when clicking on creat poll
+    const form = document.querySelector(".createForm");
+    form.style.display = "none";
+
+    // hiding dashboard and newsfeed when clicking on create poll
     const create = document.getElementById("createPoll");
     create.addEventListener("click", () => {
         const panel = document.querySelector(".panel");
         if (panel.style.display === "none") {
             panel.style.display = "block";
-            document.querySelector("#createPoll h4").innerText = "Create Poll"
+            document.querySelector("#createPoll h4").innerText = "Create Poll";
+            form.style.display = "none";
           } else {
             panel.style.display = "none";
-            document.querySelector("#createPoll h4").innerText = "Back to dashboard"
+            document.querySelector("#createPoll h4").innerText = "Back to dashboard";
+            form.style.display = "block";
           }
     })
 
-    // 
+    
 })
