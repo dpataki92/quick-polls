4097217db5e3f2922b3e65037e6f00a2e5717408
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-08-17 16:13:58 +0200

    refactoring code for creating or removing form and dashboard

--- quick-polls-frontend/src/index.js
+++ quick-polls-frontend/src/index.js
@@ -4,23 +4,29 @@ const CLOSED_POLLS_URL = `${BASE_URL}/polls/closed`
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector(".createForm");
-    form.style.display = "none";
-
-    // hiding dashboard and newsfeed when clicking on create poll
-    const create = document.getElementById("createPoll");
-    create.addEventListener("click", () => {
-        const panel = document.querySelector(".panel");
-        if (panel.style.display === "none") {
-            panel.style.display = "block";
-            document.querySelector("#createPoll h4").innerText = "Create Poll";
-            form.style.display = "none";
-          } else {
-            panel.style.display = "none";
-            document.querySelector("#createPoll h4").innerText = "Back to dashboard";
-            form.style.display = "block";
-          }
-    })
+    createOrRemoveForm();
 
     
 })
+
+// displays or hides creat poll form and dashboard based on user interaction
+function createOrRemoveForm() {
+  // hiding form for create poll by default
+  const form = document.querySelector(".createForm");
+  form.style.display = "none";
+
+  // hiding dashboard and newsfeed when clicking on create poll
+  const create = document.getElementById("createPoll");
+  create.addEventListener("click", () => {
+      const panel = document.querySelector(".panel");
+      if (panel.style.display === "none") {
+          panel.style.display = "block";
+          document.querySelector("#createPoll h4").innerText = "Create Poll";
+          form.style.display = "none";
+        } else {
+          panel.style.display = "none";
+          document.querySelector("#createPoll h4").innerText = "Back to dashboard";
+          form.style.display = "block";
+        }
+    })
+}
